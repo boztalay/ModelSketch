c04247e8bc2743ebfaf6ca8fe2e425ae5c861a0a
Ben Oztalay <boztalay@gmail.com>
2023-04-18 20:43:34 -0700

    Saving gesture images to files

--- ModelSketch/GesturesView.swift
+++ ModelSketch/GesturesView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
-struct GesturesView: View {
+enum TrainableGesture: String, CaseIterable, Identifiable {
+    var id: Self { self }
     
-    enum TrainableGesture: String, CaseIterable, Identifiable {
-        case create = "Create 'O'"
-        case delete1 = "Delete 'X' 1"
-        case delete2 = "Delete 'X' 2"
-        var id: Self { self }
+    case create
+    case delete1
+    case delete2
+    
+    var friendlyName: String {
+        switch self {
+            case .create:
+                return "Create 'O'"
+            case .delete1:
+                return "Delete 'X' 1"
+            case .delete2:
+                return "Delete 'X' 2"
+        }
     }
+}
+
+struct LabeledImage {
+    let image: UIImage
+    let label: TrainableGesture
+    let date: Date
+    
+    init(image: UIImage, label: TrainableGesture) {
+        self.image = image
+        self.label = label
+        self.date = Date()
+    }
+}
+
+struct GesturesView: View {
     
     @State private var selectedGesture = TrainableGesture.create
     @State private var strokeImage = UIImage(systemName: "pencil.line")!
+    @State private var images = [LabeledImage]()
     
     var body: some View {
         HStack {
@@ -30,7 +55,7 @@ struct GesturesView: View {
                 }
                 Picker("Gesture to Train", selection: $selectedGesture) {
                     ForEach(TrainableGesture.allCases) { gesture in
-                        Text(gesture.rawValue)
+                        Text(gesture.friendlyName)
                     }
                 }.pickerStyle(.menu)
                 HStack {
@@ -46,10 +71,18 @@ struct GesturesView: View {
     
     func strokeCompletion(_ image: UIImage) {
         strokeImage = image
+        self.images.append(LabeledImage(image: image, label: self.selectedGesture))
     }
     
     func saveImages() {
-        
+        for image in self.images {
+            let data = image.image.pngData()!
+            let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let directoryUrl = documentsUrl.appending(component: "labeled_gestures").appending(component: image.label.rawValue)
+            try! FileManager.default.createDirectory(at: directoryUrl, withIntermediateDirectories: true)
+            let fileUrl = directoryUrl.appending(path: UUID().uuidString).appendingPathExtension("png")
+            try! data.write(to: fileUrl)
+        }
     }
 }
 
