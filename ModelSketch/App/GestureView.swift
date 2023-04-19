35206b4a5e2f40be5541bb16a26c0856279a3994
Ben Oztalay <boztalay@gmail.com>
2023-04-19 11:57:44 -0700

    Add more nodes in the gesture trainer

--- ModelSketch/App/GestureView.swift
+++ ModelSketch/App/GestureView.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class GestureView: UIView {
     
+    static let nodeViewSpacing = 25.0
+    
     var pencilStrokeView: PencilStrokeView!
-    var nodeView: NodeView!
+    var nodeViews: [NodeView]!
     
     init() {
         super.init(frame: .zero)
@@ -20,16 +22,37 @@ class GestureView: UIView {
         self.pencilStrokeView.animates = false
         self.addSubview(self.pencilStrokeView)
         
-        self.nodeView = NodeView(node: Node(x: 0.0, y: 0.0))
-        self.nodeView.isUserInteractionEnabled = false
-        self.addSubview(self.nodeView)
+        self.nodeViews = [
+            NodeView(node: Node(x: 0.0, y: 0.0)),
+            NodeView(node: Node(x: 0.0, y: 0.0)),
+            NodeView(node: Node(x: 0.0, y: 0.0)),
+            NodeView(node: Node(x: 0.0, y: 0.0))
+        ]
+        
+        for nodeView in self.nodeViews {
+            nodeView.isUserInteractionEnabled = false
+            self.addSubview(nodeView)
+        }
     }
     
     override func layoutSubviews() {
         self.pencilStrokeView.frame = self.bounds
-        self.nodeView.node.x = self.center.x
-        self.nodeView.node.y = self.center.y
-        self.nodeView.update(in: self)
+        
+        self.nodeViews[0].node.x = self.center.x + GestureView.nodeViewSpacing
+        self.nodeViews[0].node.y = self.center.y + GestureView.nodeViewSpacing
+        
+        self.nodeViews[1].node.x = self.center.x - GestureView.nodeViewSpacing
+        self.nodeViews[1].node.y = self.center.y + GestureView.nodeViewSpacing
+        
+        self.nodeViews[2].node.x = self.center.x + GestureView.nodeViewSpacing
+        self.nodeViews[2].node.y = self.center.y - GestureView.nodeViewSpacing
+        
+        self.nodeViews[3].node.x = self.center.x - GestureView.nodeViewSpacing
+        self.nodeViews[3].node.y = self.center.y - GestureView.nodeViewSpacing
+        
+        for nodeView in self.nodeViews {
+            nodeView.update(in: self)
+        }
     }
     
     required init?(coder: NSCoder) {
