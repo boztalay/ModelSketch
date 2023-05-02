7a62d9a5f9769b521d4f88f7d70049c5951923aa
Ben Oztalay <boztalay@gmail.com>
2023-05-01 23:59:15 -0700

    Testing some relationships

--- ModelSketch/Model/Model.swift
+++ ModelSketch/Model/Model.swift
@@ -15,6 +15,18 @@ class Model {
     init() {
         self.constructionGraph = ConstructionGraph()
         self.metaGraph = MetaGraph(constructionGraph: self.constructionGraph)
+        
+        let nodeA = self.constructionGraph.createNode()
+        let nodeB = self.constructionGraph.createNode()
+        
+        self.constructionGraph.add(relationship: FollowPencilRelationship(node: nodeA, cgPoint: CGPoint(x: 200.0, y: 200.0)))
+        self.constructionGraph.add(relationship: FollowPencilRelationship(node: nodeB, cgPoint: CGPoint(x: 300.0, y: 300.0)))
+        self.constructionGraph.update()
+        
+        self.constructionGraph.add(relationship: DistanceRelationship(nodeIn: nodeA, nodeOut: nodeB, min: 50.0, max: 150.0))
+        self.constructionGraph.add(relationship: DistanceRelationship(nodeIn: nodeB, nodeOut: nodeA, min: 50.0, max: 150.0))
+        self.constructionGraph.add(relationship: AffixRelationship(node: nodeB, cgPoint: CGPoint(x: 300.0, y: 300.0)))
+        self.constructionGraph.update()
     }
     
     func update() {
