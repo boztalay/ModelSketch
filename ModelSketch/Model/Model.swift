8a72dbf45ecab423212c0ceb9da1a7aacbfb9b66
Ben Oztalay <boztalay@gmail.com>
2023-05-22 00:56:26 -0700

    Add a few more test nodes

--- ModelSketch/Model/Model.swift
+++ ModelSketch/Model/Model.swift
@@ -22,7 +22,17 @@ class Model {
         let nodeB = self.constructionGraph.createNode(at: CGPoint(x: 400.0, y: 400.0))
         let nodeC = self.constructionGraph.createNode(at: CGPoint(x: 500.0, y: 400.0))
         self.constructionGraph.connect(nodeA: nodeB, nodeB: nodeC)
-        self.constructionGraph.add(spring: DistanceSpring(nodeA: nodeB, nodeB: nodeC, distance: 100.0))
+        self.constructionGraph.add(spring: DistanceSpring(nodeA: nodeB, nodeB: nodeC, distance: nodeB.cgPoint.distance(to: nodeC.cgPoint)))
+        
+        let nodeD = self.constructionGraph.createNode(at: CGPoint(x: 600.0, y: 300.0))
+        let nodeE = self.constructionGraph.createNode(at: CGPoint(x: 550.0, y: 400.0))
+        let nodeF = self.constructionGraph.createNode(at: CGPoint(x: 650.0, y: 400.0))
+        self.constructionGraph.connect(nodeA: nodeD, nodeB: nodeE)
+        self.constructionGraph.connect(nodeA: nodeE, nodeB: nodeF)
+        self.constructionGraph.connect(nodeA: nodeF, nodeB: nodeD)
+        self.constructionGraph.add(spring: DistanceSpring(nodeA: nodeD, nodeB: nodeE, distance: nodeD.cgPoint.distance(to: nodeE.cgPoint)))
+        self.constructionGraph.add(spring: DistanceSpring(nodeA: nodeE, nodeB: nodeF, distance: nodeE.cgPoint.distance(to: nodeF.cgPoint)))
+        self.constructionGraph.add(spring: DistanceSpring(nodeA: nodeF, nodeB: nodeD, distance: nodeF.cgPoint.distance(to: nodeD.cgPoint)))
 
         self.update()
     }
